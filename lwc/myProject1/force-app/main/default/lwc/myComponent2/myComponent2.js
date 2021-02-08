01cfac1c6e667d9bff9a5af253cf353f5c36e3ce
Salman Khan <khan40384@gmail.com>
2021-02-08 16:36:45 +0530

    adding again

--- lwc/myProject1/force-app/main/default/lwc/myComponent2/myComponent2.js
+++ lwc/myProject1/force-app/main/default/lwc/myComponent2/myComponent2.js
@@ -1,14 +1,16 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 import SendMail from '@salesforce/apex/SendMail.Send';
 
 export default class MyComponent2 extends LightningElement {
+    @api recordId;
     sendMail(){
+        console.log(this.recordId);
         SendMail({accid: this.recordId})
         .then(result =>{
             alert(result);
         })
         .catch(error =>{
-            alert(error);
+            alert(JSON.stringify(error));
         })
     }
 }
